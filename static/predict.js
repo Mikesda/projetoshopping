d9b7305b818b5d22364b3246707f40f00bf72961
Mikesda <98934245+Mikesda@users.noreply.github.com>
2024-09-16 18:45:01 -0300

    Add files via upload

---
+++ static/predict.js
@@ -0,0 +1,79 @@
+// the link to your model provided by Teachable Machine export panel
+const URL = `tm-my-image-model/`;
+
+let model, webcam, labelContainer, maxPredictions;
+let date = [];
+let lastSpoken = "";
+
+// Load the image model and setup the webcam
+async function init() {
+  // Esconde o botão "Start" quando a função init é chamada
+  document.getElementById("start-btn").style.display = "none";
+
+  const modelURL = URL + "model.json";
+  const metadataURL = URL + "metadata.json";
+
+  // Carrega o modelo e os metadados
+  model = await tmImage.load(modelURL, metadataURL);
+  maxPredictions = model.getTotalClasses();
+
+  // Configura a webcam
+  const flip = false; // whether to flip the webcam
+  webcam = new tmImage.Webcam(400, 400, flip); // width, height, flip
+  await webcam.setup({ facingMode: "environment" }); // request access to the webcam
+  await webcam.play();
+  window.requestAnimationFrame(loop);
+
+  // Adiciona elementos ao DOM
+  document.getElementById("webcam-container").appendChild(webcam.canvas);
+  labelContainer = document.getElementById("label-container");
+  for (let i = 0; i < maxPredictions; i++) {
+    // Cria os contêineres de rótulo
+    labelContainer.appendChild(document.createElement("div"));
+  }
+}
+
+async function loop() {
+  webcam.update(); // Atualiza o quadro da webcam
+  await predict();
+  window.requestAnimationFrame(loop);
+}
+
+// Executa a imagem da webcam através do modelo de imagem
+async function predict() {
+  const prediction = await model.predict(webcam.canvas);
+  for (let i = 0; i < maxPredictions; i++) {
+    let probability = prediction[i].probability.toFixed(2); // Fix the percentage to 2 decimal places
+    if (probability > 1) probability = 1; // Ensure that the probability doesn't exceed 100%
+    if (typeof date[i] === "undefined") {
+      date[i] = new Date(2000);
+    }
+
+    const classPrediction = `${prediction[i].className}: ${(probability * 100).toFixed(0)}%`;
+
+    // Verifica se a probabilidade excede 85% para aplicar um estilo especial
+    if (probability >= 0.85) {
+      labelContainer.childNodes[
+        i
+      ].innerHTML = `<span class="highlight">${classPrediction}</span>`;
+      if (
+        new Date().getTime() - date[i].getTime() >= 5000 &&
+        prediction[i].className !== lastSpoken
+      ) {
+        date[i] = new Date();
+        if (prediction[i].className !== "Outros") {
+          responsiveVoice.speak(
+            `Estou vendo ${prediction[i].className}`,
+            "Brazilian Portuguese Female",
+            {
+              rate: 1.1,
+            }
+          );
+        }
+        lastSpoken = prediction[i].className;
+      }
+    } else {
+      labelContainer.childNodes[i].innerHTML = classPrediction;
+    }
+  }
+}
